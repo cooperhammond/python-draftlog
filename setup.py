c398a337e2e556564078136143a8a3cdc3e908b3
Kepoor Hampond <kepoorh@gmail.com>
2017-03-23 22:34:16 -0700

    Updated README and setup.py version info

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name =         'draftlog',
-    version =      '1.0.0',
+    version =      '2.0.0',
     description =  'Create updatable log lines into the terminal.',
     url =          'https://github.com/kepoorhampond/python-draftlog',
     author =       'Kepoor Hampond',
