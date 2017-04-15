be5d5f2dae0d47a32d2ee22526c26d1573148428
Samuel P. Gillispie II <spgill@vt.edu>
2017-04-15 02:47:57 -0400

    More Exceptions are required....

--- draftlog/lcs.py
+++ draftlog/lcs.py
@@ -22,7 +22,7 @@ class LineCountStream(object):
         # Reads the command "tput lines" if valid
         try:
             self.rows = subprocess.Popen("tput lines").read()
-        except (ValueError, IOError):
+        except (ValueError, IOError, OSError):
             self.rows = 20
 
     """
